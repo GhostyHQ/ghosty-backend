8434385d418a339d13bc3e8340d7009593e9e7af
Muhlis Iqbal Utomo <muhlisiqbalutomo@gmail.com>
2022-11-10 21:47:59 +0700

    feat: connect mongodb

---
+++ config/database.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const databaseConnect = () => {
+  mongoose
+    .connect(process.env.DATABASE_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log('Mongodb database is connected');
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+module.exports = databaseConnect;
--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const databaseConnect = require('./config/database');
 
 const server = express();
 const PORT = process.env.PORT || 9090;
 
+dotenv.config({ path: 'config/config.env' });
+
 server.get('/', (req, res) => {
   res.json({ status: 1 });
 });
+
+databaseConnect();
 
 server.listen(PORT, () => console.log(`server is running on port ${PORT}`));
