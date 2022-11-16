b8a0b3ee3dd735b46ccfbbac4f9fe44ba0837c2a
Muhlis Iqbal Utomo <muhlisiqbalutomo@gmail.com>
2022-11-16 18:40:41 +0700

    feat: get message

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -23,3 +23,29 @@ module.exports.sendMessage = async (req, res) => {
 		})
 	}
 }
+
+module.exports.getMessage = async (req, res) => {
+	if (req.accountId) {
+		try {
+			const currentUser = req.accountId
+			const receiverId = req.params.id
+			const getAllMessage = await Message.find({})
+
+			const currentMessage = getAllMessage.filter(
+				(message) =>
+					(message.senderId === currentUser && message.receiverId === receiverId) ||
+					(message.receiverId === currentUser && message.senderId === receiverId)
+			)
+			console.log(currentMessage)
+			res.status(200).json({
+				status: 1,
+				data: currentMessage,
+			})
+		} catch (error) {
+			res.status(500).json({
+				status: 0,
+				message: error.message,
+			})
+		}
+	}
+}
--- routes/messageRoute.js
+++ routes/messageRoute.js
@@ -1,7 +1,8 @@
 const router = require('express').Router()
 const authenticate = require('../middleware/authenticate')
-const { sendMessage } = require('../controllers/messageController')
+const { sendMessage, getMessage } = require('../controllers/messageController')
 
 router.post('/send-message', authenticate, sendMessage)
+router.get('/get-message/:id', authenticate, getMessage)
 
 module.exports = router
